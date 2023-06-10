81f390c946ed1dee4963e1ee364f18dc19cd0b2f
Riccardo Manzan <riccardo.manzan.dev@gmail.com>
2023-06-10 16:13:06 +0200

    dev: gui with fixed values

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'afds-24ore-countdown-frontend';
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params) => {
+      console.log(params)
+      this.isAuthenticated = params['isAuthenticated'];
+    });
+  }
+
+  isAuthenticated = false;
+
+  registerDonation(type: 'blood' | 'plasma', element: HTMLButtonElement) {
+    setTimeout(() => (window.document as any).activeElement?.blur(), 760);
+  }
 }
