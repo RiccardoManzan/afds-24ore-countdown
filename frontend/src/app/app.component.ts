de51a61f486d4f0b695b7e49fcfbb910f618c458
Riccardo Manzan <riccardo.manzan.dev@gmail.com>
2023-06-13 01:10:54 +0200

    fix: cors and credentials handling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,80 +1,88 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CountdownConfig, CountdownEvent, CountdownStatus } from 'ngx-countdown';
+import {
+  CountdownConfig,
+  CountdownEvent,
+  CountdownStatus,
+} from 'ngx-countdown';
 import { environment } from 'src/environments/environment';
 import { moreThan24HoursDateFormatter } from './countdown-utils';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly http: HttpClient
   ) {
-    this.route.queryParams.subscribe((params) => {
-      this.isAuthenticated = params['isAuthenticated'];
-    });
+    // this.route.queryParams.subscribe((params) => {
+    //   this.isAuthenticated = params['isAuthenticated'];
+    // });
   }
 
-  isAuthenticated = false;
+  isAuthenticated = sessionStorage.getItem('Authorization') != null;
 
-  countdownConfig : CountdownConfig = {
+  countdownConfig: CountdownConfig = {
     stopTime: environment.countdownEndDate.valueOf(),
-    formatDate: moreThan24HoursDateFormatter
-  }
-  showCountdown = false
-  isCountdownCompleted = new Date() > environment.countdownEndDate
+    formatDate: moreThan24HoursDateFormatter,
+  };
+  showCountdown = false;
+  isCountdownCompleted = new Date() > environment.countdownEndDate;
 
   ngOnInit(): void {
-    void ( async () => {
-      while(!this.showCountdown){
-        this.showCountdown = new Date > environment.countdownStartDate
-        await new Promise(r => setTimeout(r, 1000));
+    void (async () => {
+      while (!this.showCountdown) {
+        this.showCountdown = new Date() > environment.countdownStartDate;
+        await new Promise((r) => setTimeout(r, 1000));
       }
-    })()
+    })();
   }
 
-  handleCountdownEvent(event: CountdownEvent){
-    if(event.status == CountdownStatus.done){
-      this.isCountdownCompleted = true
+  handleCountdownEvent(event: CountdownEvent) {
+    if (event.status == CountdownStatus.done) {
+      this.isCountdownCompleted = true;
     }
   }
 
-  login(){
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {
-        'isAuthenticated': true
-      },
-      queryParamsHandling: 'merge',
-    });
+  login() {
+    const username = prompt('insert username');
+    const password = prompt('insert password');
+    const authorizationHeader = 'Basic ' + btoa(username + ':' + password);
+    sessionStorage.setItem('Authorization', authorizationHeader);
+
+    firstValueFrom(
+      this.http.get(`${environment.apiBasePath}/login`, {
+        headers: { Authorization: authorizationHeader },
+      })
+    )
+      .then((r) => (this.isAuthenticated = true))
+      .catch((ex) => alert('bad credentials'));
+  }
+
+  logout() {
+    sessionStorage.removeItem('Authorization');
     this.isAuthenticated = false;
   }
 
-  logout(){
-    this.http.get(`${environment.apiBasePath}/login`).subscribe();
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {
-        'isAuthenticated': undefined
-      },
-      queryParamsHandling: 'merge',
-    });
-    this.isAuthenticated = true;
-  }
-
-
   registerDonation(type: 'blood' | 'plasma', element: HTMLButtonElement) {
     setTimeout(() => (window.document as any).activeElement?.blur(), 760);
-    this.http.post(`${environment.apiBasePath}/register-donation/${type}`, {}).subscribe();
+
+    this.http
+      .post(
+        `${environment.apiBasePath}/register-donation/${type}`,
+        {},
+        {
+          headers: {
+            Authorization: sessionStorage.getItem('Authorization')!!,
+          },
+        }
+      )
+      .subscribe();
   }
-
 }
-
-
-//TODO: https://stackoverflow.com/questions/38854424/angular-http-auth-not-getting-browser-login-prompt
