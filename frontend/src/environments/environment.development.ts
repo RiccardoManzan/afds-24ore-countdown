0c99a7a65aa9cf58a4abf900eab90152531c3a59
Riccardo Manzan <riccardo.manzan.dev@gmail.com>
2024-06-12 21:17:37 +0200

    ref: removed unused code

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  // countdownStartDate: new Date('2024-05-25 11:05:30'),
-  // countdownEndDate: new Date('2024-05-26 11:05:30'),
   apiBasePath: "http://localhost:8080/api",
   counterPollingTimeout: 5000,
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  countdownStartDate: new Date('2023-06-14 12:00:00'),
-  countdownEndDate: new Date('2023-06-15 12:00:00'),
   apiBasePath: "https://afds-24ore-countdown-backend.onrender.com/api",
   counterPollingTimeout: 5000,
 };
